fb2e7ae2c7ef07920608638869783630d261ba5b
MandorSavall <bogdan.lyamzin.d@gmail.com>
2022-10-18 19:13:52 +0300

    finish lesson 9

---
+++ lesson-9/auth-example/controllers/books/removeById.js
@@ -0,0 +1,14 @@
+const {Book} = require("../../models/book")
+
+const {RequestError} = require("../../helpers")
+
+const removeById = async(req, res)=> {
+    const {id} = req.params;
+    const result = await Book.findByIdAndRemove(id);
+    if(!result){
+        throw RequestError(404, "Not found")
+    }
+    res.json(result);
+}
+
+module.exports = removeById;
